b255f7c244116a1ee70707e2f1455ab7985f0ffd
kogle <kogler95@gmail.com>
2022-06-03 14:48:18 +0200

    Text To Speech Works

--- lib/pages/topics_learn_page.dart
+++ lib/pages/topics_learn_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:mms_interval_learning/providers/question_provider.dart';
 import 'package:mms_interval_learning/widgets/question_view.dart';
 
@@ -15,12 +16,26 @@ class TopicsLearnPage extends ConsumerStatefulWidget {
 }
 
 class _TopicsLearnPageState extends ConsumerState<TopicsLearnPage> {
+  FlutterTts flutterTts = FlutterTts();
+  bool playing = false;
   Question? currentQuestion;
 
   @override
   void initState() {
     super.initState();
     ref.read(questionProvider.notifier).setQuestionsForTopics(widget.topicIds);
+    flutterTts.setLanguage("de-AT");
+  }
+
+  Future _speak(String question) async {
+    setState(() => playing = true);
+    var result = await flutterTts.speak(question);
+    if (result == 1) setState(() => playing = false);
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => playing = false);
   }
 
   @override
@@ -33,13 +48,26 @@ class _TopicsLearnPageState extends ConsumerState<TopicsLearnPage> {
       ),
       body: watch.isEmpty
           ? null
-          : QuestionView(
-              onLearned: () {
-                ref
-                    .read(questionProvider.notifier)
-                    .markQuestionAsLearned(watch.first);
-              },
-              question: watch.first),
+          : Row(
+              children: [
+                QuestionView(
+                    onLearned: () {
+                      ref
+                          .read(questionProvider.notifier)
+                          .markQuestionAsLearned(watch.first);
+                    },
+                    question: watch.first),
+                TextButton(
+                    onPressed: () {
+                      if (playing) {
+                        _stop();
+                      } else {
+                        _speak(watch.first.text);
+                      }
+                    },
+                    child: Icon(Icons.play_arrow))
+              ],
+            ),
     );
   }
 }
